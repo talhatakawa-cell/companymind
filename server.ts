0bd42bb830b5d0689828d232e5d3bace3065f9f8
rafi <talhatakawa@gmail.com>
2026-04-09 14:09:54 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 3000;
 
 // Supabase Setup
 const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
